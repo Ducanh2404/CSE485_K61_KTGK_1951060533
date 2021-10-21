7389c28ff6cb4318a7d5665e868629a863e80648
Ducanh2404 <1951060533@e.tlu.edu.vn>
2021-10-21 12:57:46 +0700

    Thay doi

--- details.php
+++ details.php
@@ -3,7 +3,7 @@
 <main>
     <?php
     $reci_id = $_GET['reci_id'];
-    $query = mysqli_query($conn, "select * from blood_recipient where reci_id='$reci_id'");
+    $query = mysqli_query($conn, "SELECT * from blood_recipient where reci_id='$reci_id'");
     $row = mysqli_fetch_assoc($query);
     ?>
     <div class="container-fluid">
@@ -12,31 +12,31 @@
             <div class="form-group row">
                 <label class="col-sm-2 col-form-label">Họ và Tên </label>
                 <div class="col-sm-10">
-                    <input type="text" value="<?php echo $row['reci_name']; ?>" name="name">
+                    <input type="text" value="<?php echo $row['reci_name']; ?>" name="reci_name">
                 </div>
-            
+            </div>
             <div class="form-group row">
                 <label class="col-sm-2 col-form-label">Năm Sinh </label>
                 <div class="col-sm-10">
-                    <input type="text" value="<?php echo $row['reci_age']; ?>" name="age">
+                    <input type="text" value="<?php echo $row['reci_age']; ?>" name="reci_age">
                 </div>
             </div>
             <div class="form-group row">
                 <label class="col-sm-2 col-form-label">Nhóm Máu </label>
                 <div class="col-sm-10">
-                    <input type="text" value="<?php echo $row['reci_bgrp']; ?>" name="bgrp">
+                    <input type="text" value="<?php echo $row['reci_bgrp']; ?>" name="reci_bgrp">
                 </div>
             </div>
             <div class="form-group row">
                 <label class="col-sm-2 col-form-label">Số Lượng Máu Cần Nhận </label>
                 <div class="col-sm-10">
-                <input type="text" value="<?php echo $row['reci_bqnty']; ?>" name="bqnty">
+                <input type="text" value="<?php echo $row['reci_bqnty']; ?>" name="reci_bqnty">
                 </div>
             </div>
             <div class="form-group row">
                 <label class="col-sm-2 col-form-label">Giới Tính </label>
                 <div class="col-sm-10">
-                <input type="text" value="<?php echo $row['reci_sex']; ?>" name="sex"><br>
+                <input type="text" value="<?php echo $row['reci_sex']; ?>" name="reci_sex"><br>
                 </div>
             </div>
             <div class="form-group row">
@@ -48,12 +48,14 @@
             <div class="form-group row">
                 <label class="col-sm-2 col-form-label">Số Điện Thoại</label>
                 <div class="col-sm-10">
-                <input type="text" value="<?php echo $row['reci_phno']; ?>" name="phone"><br>
+                <input type="text" value="<?php echo $row['reci_phno']; ?>" name="reci_phno"><br>
                 </div>
             </div>
             
             <input type="submit" value="sửa" name="update">
-            <td><a href="delete.php?reci_id=<?php echo $row['reci_id']; ?>"><i class="fas fa-trash"></i></a></td>
+            
         </form>
+        <td><a href="delete.php?reci_id=<?php echo $row['reci_id']; ?>"><i class="fas fa-trash"></i></a></td>
     </div>
+
 </main>
